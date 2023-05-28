425be265d2ce2e239e8fcd01db64722c48e7511f
mertaydin9999 <mertaydin9999@hotmail.com>
2023-05-28 03:36:58 +0300

    com

--- project/src/components/AdvertDetail.jsx
+++ project/src/components/AdvertDetail.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useFetchAdvertsQuery } from "../store";
 import "../styles/AdvertDetail.css";
+import EmailIcon from "@mui/icons-material/Email";
 function AdvertDetail() {
   const { id } = useParams();
   const { data, isError, isFetching } = useFetchAdvertsQuery();
@@ -16,73 +17,101 @@ function AdvertDetail() {
       ?.filter((advertDetail) => advertDetail.id == id)
       .map((advertDetail) => {
         return (
-          <div className="general-div" key={advertDetail.id}>
-            <div className="top-side">
-              <div className="left-div">
-                <div className="photos-div">
-                  <div className="left-photo-div">
-                    <div className="big-image-div">
-                      <img src={advertDetail.imageUrl} alt="" />
+          <div>
+            <div className="general-div" key={advertDetail.id}>
+              <div className="left-root">
+                <Link to="/advert">Ilanlar'a git</Link>
+                <Link to="/my-adverts">Ilanlarim</Link>
+                <Link to="/create-advert">Ilan Olustur</Link>
+              </div>
+              <div className="right-root">
+                <div>
+                  <h3 className="advert-detail-header">Ilan Detaylari</h3>
+                </div>
+                <div className="top-side">
+                  <div className="left-div">
+                    <div className="photos-div">
+                      <div className="left-photo-div">
+                        <div className="big-image-div">
+                          <img src={advertDetail.imageUrl} alt="" />
+                        </div>
+                        <div className="small-images-div">
+                          <img
+                            className="small-image"
+                            src={advertDetail.imageUrl}
+                            alt=""
+                          />
+                          <img
+                            className="small-image"
+                            src={advertDetail.imageUrl}
+                            alt=""
+                          />
+                          <img
+                            className="small-image"
+                            src={advertDetail.imageUrl}
+                            alt=""
+                          />
+                          <img
+                            className="small-image"
+                            src={advertDetail.imageUrl}
+                            alt=""
+                          />
+                          <img
+                            className="small-image"
+                            src={advertDetail.imageUrl}
+                            alt=""
+                          />
+                        </div>
+                      </div>
                     </div>
-                    <div className="small-images-div">
-                      <img
-                        className="small-image"
-                        src={advertDetail.imageUrl}
-                        alt=""
-                      />
-                      <img
-                        className="small-image"
-                        src={advertDetail.imageUrl}
-                        alt=""
-                      />
-                      <img
-                        className="small-image"
-                        src={advertDetail.imageUrl}
-                        alt=""
-                      />
-                      <img
-                        className="small-image"
-                        src={advertDetail.imageUrl}
-                        alt=""
-                      />
-                      <img
-                        className="small-image"
-                        src={advertDetail.imageUrl}
-                        alt=""
-                      />
+                  </div>
+                  <div className="right-div">
+                    <div className="left-title-div">{advertDetail.title}</div>
+                    <div>
+                      <label className="price-label">Fiyat</label>
+                      <span className="price">{advertDetail.price}</span>
+                    </div>
+                    <div>
+                      <label className="customer-label">Ilan Sahibi</label>
+                      <span className="price">Mert Aydin</span>
+                    </div>
+                    <div>
+                      <label className="address-label">Adres</label>
+                      <span className="address">{advertDetail.address}</span>
+                    </div>
+                    <div>
+                      <button>
+                        Mesaj At
+                        <EmailIcon sx={{ paddingLeft: 1, fontSize: 30 }} />
+                      </button>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="right-div">
-                <div className="left-title-desc-div">{advertDetail.title}</div>
-                <div>Ilanlara Don</div>
-                <div className="right-price-div">{advertDetail.price}</div>
-                <div className="right-owner-div">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde
-                  deleniti, minima optio asperiores accusamus ea voluptatum
+                <div className="bottom-div">
+                  <div className="bottom-advert-desc">
+                    <div>
+                      <h4 className="details">Detayli Aciklama</h4>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Sequi amet deleniti exercitationem. Ipsum, possimus
+                      officiis animi sed nihil, nostrum quia inventore facilis
+                      iste molestiae, tenetur in amet officia error eos.
+                      <br />
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Sed ullam voluptas rerum. Delectus veritatis soluta ad
+                      maxime dolore omnis quas dolorem nemo pariatur! Atque
+                      consectetur tempora neque esse porro quidem!
+                    </p>
+                  </div>
                 </div>
-                <div className="right-loc-div">{advertDetail.address}</div>
               </div>
             </div>
-            <div>
-              <div className="bottom-advert-desc">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
-                amet deleniti exercitationem. Ipsum, possimus officiis animi sed
-                nihil, nostrum quia inventore facilis iste molestiae, tenetur in
-                amet officia error eos.
-                <br />
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
-                ullam voluptas rerum. Delectus veritatis soluta ad maxime dolore
-                omnis quas dolorem nemo pariatur! Atque consectetur tempora
-                neque esse porro quidem!
-              </div>
-              <div className="bottom-similar-advert">
-                <img src={advertDetail.imageUrl} alt="" />
-                <img src={advertDetail.imageUrl} alt="" />
-                <img src={advertDetail.imageUrl} alt="" />
-                <img src={advertDetail.imageUrl} alt="" />
-              </div>
+            <div className="bottom-similar-advert">
+              <img src={advertDetail.imageUrl} alt="" />
+              <img src={advertDetail.imageUrl} alt="" />
+              <img src={advertDetail.imageUrl} alt="" />
+              <img src={advertDetail.imageUrl} alt="" />
             </div>
           </div>
         );
